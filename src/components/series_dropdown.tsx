7f255990a5e08ab5062330972f59a962d6a7e52b
xsj3n <xsj3n@tutanota.com>
2026-03-07 09:12:55 -0700

    tag bubble was not being used, tweaked color

--- src/components/series_dropdown.tsx
+++ src/components/series_dropdown.tsx
@@ -4,19 +4,7 @@ import { Series } from "./posts"
 import { fixedsys } from "./fonts"
 import Link from "next/link"
 import { RiArrowDropDownLine } from "react-icons/ri"
-
-
-function TagBubbles(tags: string[] | undefined) {
-  if (tags === undefined) {
-    return (<></>)
-  }
-  
-  return (
-    <div className="flex justify-center gap-4 mt-4 mb-2">
-      {tags.map((tag) => (<div key={tag} className="outline outline-(--background) rounded-lg outline-offset-4 text-sm">{tag}</div>))}
-    </div>
-  )
-}
+import TagBubbles from "@/components/tag_bubbles"
 
 // the hyphe in the filename may be messing with the dynamic route, may have to adjust 
 export default function SeriesDropdowns(props: Series & {className?: string}){ 
--- src/components/tag_bubbles.tsx
+++ src/components/tag_bubbles.tsx
@@ -1,5 +1,3 @@
-
-
 export default function TagBubbles(tags: string[] | undefined) {
   if (tags === undefined) {
     return (<></>)
@@ -7,7 +5,7 @@ export default function TagBubbles(tags: string[] | undefined) {
   
   return (
     <div className="flex justify-center gap-4 mt-4 mb-2">
-      {tags.map((tag) => (<div key={tag} className="outline outline-(--dark-background) rounded-lg outline-offset-4 text-sm">{tag}</div>))}
+      {tags.map((tag) => (<div key={tag} className="outline outline-gray-500 rounded-lg outline-offset-4 text-sm">{tag}</div>))}
     </div>
   )
 }
