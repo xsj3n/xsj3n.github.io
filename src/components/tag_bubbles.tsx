21d310e5aeafd3b2d013c9ea03f563efb5f493cd
xsj3n <xsj3n@tutanota.com>
2026-03-07 01:38:25 -0700

    font size change, color changes

--- src/components/tag_bubbles.tsx
+++ src/components/tag_bubbles.tsx
@@ -7,7 +7,7 @@ export default function TagBubbles(tags: string[] | undefined) {
   
   return (
     <div className="flex justify-center gap-4 mt-4 mb-2">
-      {tags.map((tag) => (<div key={tag} className="outline outline-(--background) rounded-lg outline-offset-4 text-sm">{tag}</div>))}
+      {tags.map((tag) => (<div key={tag} className="outline outline-(--dark-background) rounded-lg outline-offset-4 text-sm">{tag}</div>))}
     </div>
   )
 }
